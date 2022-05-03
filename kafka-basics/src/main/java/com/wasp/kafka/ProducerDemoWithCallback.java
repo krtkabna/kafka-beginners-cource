4a30873931ccb0b68804014f216ad01de2e36661
wasp <wasp@rocke.tech>
2022-05-03 13:51:25 +0300

    create ProducerDemoWithCallback

---
+++ kafka-basics/src/main/java/com/wasp/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,53 @@
+package com.wasp.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+@Slf4j
+public class ProducerDemoWithCallback {
+
+    public static void main(String[] args) {
+        log.info("Hi from Kafka Producer");
+
+        //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the Producer
+        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+            for (int i = 0; i < 10; i++) {
+
+                //create producer record
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world" + i);
+
+                //send data - async
+                producer.send(producerRecord, logMetadataCallback());
+            }
+
+            //flush - synchronous - and close producer
+        }
+    }
+
+    private static Callback logMetadataCallback() {
+        return (metadata, exception) -> {
+            //executes every time a record is successfully sent or an exception is thrown
+            if (exception == null) {
+                //success - no exception
+                logMetadata(metadata);
+            } else log.error("Error while producing", exception);
+        };
+    }
+
+    private static void logMetadata(RecordMetadata metadata) {
+        log.info("Received new metadata:\n" +
+                "topic: " + metadata.topic() + "\n" +
+                "partition: " + metadata.partition() + "\n" +
+                "offset: " + metadata.offset() + "\n" +
+                "timestamp: " + metadata.timestamp());
+    }
+}
