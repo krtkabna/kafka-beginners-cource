1881b30e93d68a8ca3a7778b0c05692bbd74191c
wasp <wasp@rocke.tech>
2022-05-06 16:22:15 +0300

    create Wikimedia producer module

---
+++ kafka-producer-wikimedia/src/main/java/com/wasp/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,45 @@
+package com.wasp.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+@Slf4j
+public class WikimediaChangeHandler implements EventHandler {
+    private final KafkaProducer<String, String> kafkaProducer;
+    private final String topic;
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+        // nothing here
+    }
+
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) {
+        log.info(messageEvent.getData());
+        // asynchronous
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) {
+        // nothing here
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error("Error in Stream Reading", t);
+    }
+}
